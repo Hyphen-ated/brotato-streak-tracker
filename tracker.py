192308f1b59e30064298990d0dfc28e200f2b2be
Hyphen-ated <Hyphen-ated@users.noreply.github.com>
2023-07-10 23:06:16 -0700

    add some more weapon text replacements, and unfinished timer code

--- tracker.py
+++ tracker.py
@@ -1,13 +1,27 @@
 import time
+
 brotato_dir = "C:/Users/s/AppData/Roaming/Brotato/76561197960788425/"
 logfile = brotato_dir + "log.txt"
 tracking_file = "obs_streak_track.txt"
+
+#todo: call this
+def format_time(start_time):
+    if start_time is None:
+        return ""
+    duration = int(time.time() - start_time)
+    hours, remainder = divmod(duration, 3600)
+    minutes, seconds = divmod(remainder, 60)
+    output = "{:02}:{:02}".format(int(minutes), int(seconds))
+    if hours > 0:
+        output = str(int(hours)) + output
+    return "--" + output
 
 old_wave = ""
 new_char = ""
 new_wave = ""
 starting_weapons = ""
 finished_logging_run = False
+start_time = None
 with open(tracking_file, "r") as outputfile:
     out_lines = outputfile.read().splitlines(keepends=True)
     if len(out_lines) == 0:
@@ -29,6 +43,9 @@ while True:
                 starting_weapons = line[len("Weapons: "):].strip()
                 starting_weapons = starting_weapons.replace("weapon_", "")
                 starting_weapons = starting_weapons.replace("_1", "")
+                starting_weapons = starting_weapons.replace("jousting_", "")
+                starting_weapons = starting_weapons.replace("double_barrel_", "")
+                starting_weapons = starting_weapons.replace("minigun_3", "")
                 if len(starting_weapons) > 0 and starting_weapons[-1] == ',':
                         starting_weapons = starting_weapons[0:-1]
             if line.startswith("is_run_won"):
@@ -36,13 +53,15 @@ while True:
             if line.startswith("is_run_lost"):
                 run_lost = True
 
+        change = False
         # there are three reasons to edit the output: a new run started, a run ended in win, or a run ended in loss
         if new_wave == "1" and old_wave != "1":
             print("new run detected with " + new_char)
+            start_time = time.time()
             out_lines.insert(0, new_char + ", " + starting_weapons + "\n")
             change = True
         elif run_won:
-            out_lines[0] = new_char + ", " + starting_weapons + ": Won\n"
+            out_lines[0] = new_char + ", " + starting_weapons + ": Won" + "\n"
             change = True
         elif run_lost:
             out_lines[0] = new_char + ", " + starting_weapons + ": w" + new_wave + "\n"
